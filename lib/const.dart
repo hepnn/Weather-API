4fb025906c1333d76c1e9990efbc70d276e4cd87
hepnn <47219299+hepnn@users.noreply.github.com>
2022-04-11 19:56:08 +0300

    Update const.dart

--- lib/const.dart
+++ lib/const.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const API_KEY = 'b99ba13988cb8923240829f236b42b41';
+const API_KEY = 'your api key(openweatherapi)';
 
 const kDetailScreenTxtStyle = TextStyle(
   color: Colors.white,
